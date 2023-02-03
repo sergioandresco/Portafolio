e4a874b043cf83ccda972511755b4c3d792ca092
sergioandresco <sergiocobos6@outlook.com>
2023-02-02 20:47:21 -0500

    Creación e instalación de crud desde consola

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -54,6 +54,7 @@
         <!-- Portfolio Section-->
         <section class="page-section portfolio" id="portfolio">
             <div class="container">
+                @yield('content')
                 <!-- Portfolio Section Heading-->
                 <h2 class="page-section-heading text-center text-uppercase text-secondary mb-0">Portfolio</h2>
                 <!-- Icon Divider-->
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,1 +1,6 @@
 @extends('layouts.template');
+
+@section('content')
+
+<center><h1>Esto es una prueba de YIELD</h1></center>
+@endsection
