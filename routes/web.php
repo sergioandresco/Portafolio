25cd8f305afca56bcad956aa38101dd823f2750e
sergioandresco <sergiocobos6@outlook.com>
2023-02-02 22:32:47 -0500

    Creacion del CRUD con instalacion artisan

--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
 Auth::routes();
+
+Route::resource('/proyectos', App\Http\Controllers\ProyectoController::class);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
